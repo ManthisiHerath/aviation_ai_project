6a88d6262e99b1e027a2ecf4d766638baaf3f6f7
Manthisi Herath <manthisi.h@egmail.com>
2026-02-20 09:50:34 +0530

    Updated main.py with new changes

--- code/main.py
+++ code/main.py
@@ -7,23 +7,35 @@ print(df.shape) #how many flights you have  #how many columns (features)
 print("\n-------------------------------\n")
 print(df.columns) #list of all column names
 print("\n-------------------------------\n")
-print(df.isnull().sum())
+print(df.isnull().sum()) # Check for missing values in each column
 print("\n-------------------------------\n")
 print(df.describe())
+print("\n-------------------------------\n\n")
 
-
+'''
+     We can drop the "DelayReason" column as it contains categorical data 
+     that may not be directly useful for our analysis.
+'''
 df1 = df.drop(columns=["DelayReason"])
 print(df1.isnull().sum())
-print("\n--- Converting date & time columns ---\n")
+print("\n-------------------------------\n")
+# Converting date & time columns to datetime format
+# Convert the "ScheduledDeparture" column to datetime format
 df1["ScheduledDeparture"] = pd.to_datetime(df1["ScheduledDeparture"])
+# Convert the "ActualDeparture" column to datetime format
 df1["ActualDeparture"] = pd.to_datetime(df1["ActualDeparture"])
+# Convert the "ScheduledArrival" column to datetime format
 df1["ScheduledArrival"] = pd.to_datetime(df1["ScheduledArrival"])
+# Convert the "ActualArrival" column to datetime format
 df1["ActualArrival"] = pd.to_datetime(df1["ActualArrival"])
+# Calculate the departure delay and flight duration in minutes
 df1["DepartureDelay"] = (df1["ActualDeparture"] - df1["ScheduledDeparture"]).dt.total_seconds() / 60
 df1["FlightDuration"] = (df1["ActualArrival"] - df1["ActualDeparture"]).dt.total_seconds() / 60
-print("\n--- After processing date & time ---\n")
+''' 
+   After processing date & time columns, we can check the first 
+   few rows and the info of the DataFrame again
+'''
 print(df1.head())
+print("\n-------------------------------\n")
 print(df1.info())
-
-
-print("\n-------------  Day 003 - 12/21/2025  ------------------\n")
+print("\n-------------------------------\n\n")
